ca36a6abc16947d5f57d9a63d8430f2585b64463
Deiverguerrero96 <89583628+jotapeco988@users.noreply.github.com>
2021-09-04 13:08:15 -0500

    CREADO EN CLASE

--- servicio.js
+++ servicio.js
@@ -1,5 +1,4 @@
 let uri="https://api.spotify.com/v1/artists/3YcBF2ttyueytpXtEzn1Za/top-tracks?market=us";
-
 let token="Bearer BQDlZPSEBZdCLzyOPqmLv6NBoozoytzW_3wmhAeJgIptRlVjPnvu0zeor1QHadxzJ0wJWF55fw0XQld1kpUEVXJgiSbOmXHjyl_873_wqICvFP7u-Vc5gz3gpKe1y8SbJpDXtHqf64XWxqZQU300MiCC7wZSFqg";
 
 let parametrosPeticion={
@@ -7,7 +6,6 @@ let parametrosPeticion={
  headers:{
     Authorization:token
  }
-
 }
 
 fetch(uri,parametrosPeticion)
@@ -16,10 +14,40 @@ fetch(uri,parametrosPeticion)
 })
 .then(function(respuesta){
     console.log(respuesta);
-    console.log(respuesta.tracks);
-    console.log(respuesta.tracks[0]);
-    console.log(respuesta.tracks[0].name);
+    pintarDatos(respuesta.tracks);
+    // console.log(respuesta.tracks);
+    // console.log(respuesta.tracks[0]);
+    // console.log(respuesta.tracks[0].name);
+    // console.log(respuesta.tracks[0].preview_url);
+    // console.log(respuesta.tracks[0].album.images[0].url);
 })
 .catch(function(error){
     console.log(error);
 })
+
+function pintarDatos(datos){
+    let fila=document.getElementById("fila");
+    datos.forEach(function(cancion){
+        let columna=document.createElement("div");
+        columna.classList.add("col");
+
+        let tarjeta=document.createElement("div");
+        tarjeta.classList.add("card");
+        tarjeta.classList.add("h-100");
+
+        let imagen = document.createElement("img");
+        imagen.classList.add("card-img-top");
+        imagen.src=cancion.album.images[0].url;
+
+        let titulo = document.createElement("h5");
+        titulo.textContent=cancion.name;
+
+        //PADRES E HIJOS 
+
+        tarjeta.appendChild(imagen);
+        tarjeta.appendChild(titulo);
+        columna.appendChild(tarjeta);
+        fila.appendChild(columna);
+    })
+    
+}

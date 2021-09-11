6628a065d30aa92e9453f610e3f8fe955fc14686
mvfrank <velezmauricio13@gmail.com>
2021-09-11 11:45:44 -0500

    en clase

--- servicio.js
+++ servicio.js
@@ -1,5 +1,5 @@
 let uri="https://api.spotify.com/v1/artists/3YcBF2ttyueytpXtEzn1Za/top-tracks?market=us";
-let token="Bearer BQDlZPSEBZdCLzyOPqmLv6NBoozoytzW_3wmhAeJgIptRlVjPnvu0zeor1QHadxzJ0wJWF55fw0XQld1kpUEVXJgiSbOmXHjyl_873_wqICvFP7u-Vc5gz3gpKe1y8SbJpDXtHqf64XWxqZQU300MiCC7wZSFqg";
+let token="Bearer BQDiFXCXFK7uMreuVq-NUqDpb00w6QMLqdOKLbe66dED7q34eK5LkL_-6dg6LQf1wiilJVeZwioQzjReAWxkUcAj-6fG7WzlO8iTS-Bi-22Ydn3gqXj63JdpoDE4PPCsTYH9a-Zz2WqbOCiiYf830fHPwcU1_FM";
 
 let parametrosPeticion={
  method:"GET",
@@ -26,12 +26,15 @@ fetch(uri,parametrosPeticion)
 })
 
 function pintarDatos(datos){
+    //Punto de apoyo
     let fila=document.getElementById("fila");
     datos.forEach(function(cancion){
+        //crear un div con js
         let columna=document.createElement("div");
         columna.classList.add("col");
 
         let tarjeta=document.createElement("div");
+        //crear clases para diseño
         tarjeta.classList.add("card");
         tarjeta.classList.add("h-100");
 
@@ -39,23 +42,33 @@ function pintarDatos(datos){
         imagen.classList.add("card-img-top");
         imagen.src=cancion.album.images[0].url;
 
+        //crear audio
+        let audio = document.createElement("audio")
+        audio.classList.add("w-100")
+        audio.setAttribute("controls","controls")
+        audio.src=cancion.preview_url
+
         let titulo = document.createElement("h5");
         titulo.textContent=cancion.name;
 
         //PADRES E HIJOS 
-
         tarjeta.appendChild(imagen);
         tarjeta.appendChild(titulo);
+        tarjeta.appendChild(audio)
         columna.appendChild(tarjeta);
         fila.appendChild(columna);
+        
     })
     
 }
 
 /*
-Api = Mesero, permite cambio de cliiente con servidor (cambio de datos cliente-servidor)
+Api = Mesero, permite cambio de cliente con servidor (cambio de datos cliente-servidor)
 Peticion = Cliente al servidor 2 elementos (cabecera y cuerpo) cabecera= uri (a donde voy (einpot= servicio especifico a donde estoy accediendo) 4 tipos de metodos
 get, post, put, delete) cuando el cliente llegue tengo que tener datos Json(objeto) donde guardo mucha informacion (atributo y valor)
-Respuenta = 
+feach = promesa (datos al servidor) el elemento .then(verificar que este en formato json) (catch = donde hay error )
+Respuenta = para dodne voy (uri) con parametros 
+
+pogressVar  
 
 */
---
+++ servicio2.js
@@ -0,0 +1,5 @@
+let uri = "https://accounts.spotify.com/api/token";
+
+let dato1="grant_type=";
+let dato2;
+let dato3;
